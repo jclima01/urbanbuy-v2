f6c7535618133599cf5039b12843d41bfdb2cc44
Juan Cruz <juanlimacaramico@gmail.com>
2023-08-16 20:48:04 -0300

    adding dasboardProducts structure

--- client/src/components/DashboardComponents/ProductsDashboard/ProductsDashboard.jsx
+++ client/src/components/DashboardComponents/ProductsDashboard/ProductsDashboard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 const products = [
   {
     id: "1",
@@ -7,28 +6,43 @@ const products = [
     image:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
   },
+  {
+    id: "2",
+    name: "jc",
+    price: "100",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
+  {
+    id: "3",
+    name: "jc",
+    price: "100",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
 ];
+
 const ProductsDashboard = ({ clientAdmin }) => {
   return (
-    <section className="w-full h-full pt-[133px] p-10 flex flex-col gap-y-5 overflow-y-scroll">
-      <header className="w-full flex flex-col md:flex-row items-center justify-center  gap-x-10 ">
-        <div className="box w-full h-full md:w-fit flex flex-col justify-center items-center">
+    <section className="w-full h-full pt-[140px] px-2 flex flex-col items-start gap-y-5  box-border">
+      <header className="w-full md:flex  items-center justify-center  gap-y-5 gap-x-5 ">
+        <div className="box w-full h-fit md:w-fit md:h-full flex flex-col justify-center items-center m-2">
           <span className="font-bold">Productos Totales</span>
           <span className="font-semibold">10</span>
         </div>
-        <div className="box flex flex-col w-full h-full text-gray-100 bg-[#B26E07] flex-grow ">
+        <div className="box flex flex-col h-fit md:h-full text-gray-100 bg-[#B26E07] flex-grow m-2">
           <span>Categorías:</span>
           {products?.map((product) => (
             <span className="">{clientAdmin?.category}</span>
           ))}
         </div>
-        <div className=" w-full md:w-fit flex flex-col items-center justify-center gap-y-2">
+        <div className="w-full md:w-fit flex flex-col items-center justify-center gap-y-2">
           <button className="secondaryButton"> Agregar Producto</button>
           <button className="secondaryButton"> Editar Categorías</button>
         </div>
       </header>
 
-      <nav className="flex gap-x-5">
+      <nav className="flex w-full gap-x-5 items-center justify-center ">
         <button className="secondaryButton">Seleccione</button>
         <input
           type="text"
@@ -36,16 +50,28 @@ const ProductsDashboard = ({ clientAdmin }) => {
           placeholder="Buscar producto"
         />
       </nav>
-      <div className="box flex w-full h-full items-start justify-center ">
-        {products?.map((product) => (
-          <div
-            key={product.id}
-            className="box w-full h-fit flex justify-center items-center"
-          >
-            <span className="font-bold">{product.name}</span>
-            <span className="font-semibold">{product.price}</span>
-          </div>
-        ))}
+      <div className="box w-full ">
+        <ul className="w-full bg-[#D9D9D9] rounded-md p-3 flex flex-col gap-y-4 justify-center ">
+          {products?.map((product,i) => (
+            <li
+              key={product.id}
+              className={`flex flex-row  items-center  rounded-md p-2 border-[4px] ${
+                i % 2 === 0 && "bg-[#FFFFFF]"
+              }`}
+            >
+              <img src={product.image} alt="" className="w-8 rounded-full h-auto" />
+              <span className="w-1/4 ">{product.name}</span>
+              <span className="w-1/4 ">Categoría</span>
+              <span className="w-1/4 font-semibold">{product.price}</span>
+              <div className="w-1/4 flex flex-col items-center">
+                <button className="secondaryButton">Borrar</button>
+
+                <button className="primaryButton">Editar</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+       
       </div>
     </section>
   );
